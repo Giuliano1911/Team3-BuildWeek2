5a0248f18f637cd56e2ca0e8f4b5dbfea519922c
Giuliano1911 <giulianotorrisi1911@yahoo.com>
2024-11-18 15:25:15 +0100

    starts tests on APIs

---
+++ script/homepage.js
@@ -0,0 +1,19 @@
+const url =
+  'https://striveschool-api.herokuapp.com/api/deezer/search?q=ironmaiden'
+
+fetch(url)
+  .then((response) => {
+    console.log(response)
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw new Error('No ok')
+    }
+  })
+  .then((data) => {
+    console.log(data)
+    // Qui posso lavorare con il dato preso dal server
+  })
+  .catch((err) => {
+    console.log('errore', err)
+  })
